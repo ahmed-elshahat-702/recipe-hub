e359a6166f326fb211d719fd600a46197b595387
ahmed-elshahat-702 <ahmedelshahat702@gmail.com>
2024-12-03 09:50:35 +0200

    fix auth error

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,6 @@
-import NextAuth from "next-auth/next";
+import NextAuth from "next-auth";
 import { authOptions } from "../auth-options";
 
 const handler = NextAuth(authOptions);
 
-export const { GET, POST } = handler;
+export { handler as GET, handler as POST };
